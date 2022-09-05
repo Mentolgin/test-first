3cfef4a9e7d8784bb2099d084cf4086e979c1615
mentolgin <mentolgin@gmail.com>
2022-09-05 18:14:20 +0400

    initial commit

---
+++ html/script.js
@@ -0,0 +1,12 @@
+const target = document.querySelector('#target');
+
+let a = 10;
+let b = 'stroka';
+let c = true;
+let d = [1,2,3,4,5];
+
+const human = {
+    name: "kerim",
+    age: 29,
+    profession: 'gay'
+}
